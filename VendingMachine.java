f28bcdc5418b3b425d64972faf75ac84ea02a992
cato11087 <33876060+cato11087@users.noreply.github.com>
2017-11-22 01:44:34 -0500

    updated vending machine project
    i've added a bit more to the GUI for the project. It's a rough version, so feel free to edit/clean the code up if you'd like. I've been working on this for a couple of hours. I'm gonna call it quits for the night. I couldn't figure out how to use an array and then have it appear has JButtons on the left hand side. I've marked where it is within the code. let me know if you have any questions. thanks

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,25 +1,64 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class VendingMachine {
+import java.util.*;
+public class VendingMachine extends JFrame {
   
-  
-  public static void main(String[] args) { 
+  public static void main(String[] args) {
     
-    
-    JFrame vending = new JFrame();
+    //GUI frame for vending machine
+     JFrame vending = new JFrame();
     
     vending.setTitle("Vending Machine");
-    vending.setSize(400,800);
+    vending.setSize(500,900);
     vending.setLocationRelativeTo(null);
     
-    JTextField itemPrice = new JTextField();
-    itemPrice.setHorizontalAlignment(JTextField.RIGHT);
+    //screen prompt
+    JTextField userPrompt = new JTextField("Please Make A Selection!");
+    userPrompt.setPreferredSize(new Dimension(100, 24));
+    userPrompt.setHorizontalAlignment(JTextField.CENTER);
     
-    vending.add(itemPrice, BorderLayout.NORTH);
+    //button layout for user input
+    JPanel buttonPanel = new JPanel();
+    
+    buttonPanel.setLayout(new GridLayout(5,3));
+    buttonPanel.setPreferredSize(new Dimension(250, 100));
+    vending.add(buttonPanel, BorderLayout.EAST);
+    
+        for(char i = 'A'; i <= 'K'; i++)
+    {
+    buttonPanel.add(new JButton(String.valueOf(i)));
+    }
+        for(int i = 1; i <= 10; i++) 
+    {
+    buttonPanel.add(new JButton(String.valueOf(i%10)));
+    }
+        
+
+     
+    //panel for items
+    JPanel itemPanel = new JPanel();
+    itemPanel.setLayout(new GridLayout(4,1));
+    vending.add(itemPanel, BorderLayout.WEST);
+    /*
+     * **I can't seem to connect an array to a JButton and have it appear
+     * on the panel. Think you could check this out?
+     * 
+    //buttons for items & Arrays for items
+     JButton itemButtons = new JButton[5];
+      
+        //Array for items
+  String[] items = {"Beverage1", "Snack1", "Beverage2", "Snack2",};
+    
+    for (int i = 0; i < items.length; i++)
+    {
+     itemButtons[i] = new JButton(items[i]);
+    }
+    
+    */
     
     
-    
+    vending.add(userPrompt, BorderLayout.NORTH);
     vending.setVisible(true);
   }
   
